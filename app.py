bd6aacb1fd559fe888e051948ada1551f2665de9
ymm <geekreading@163.com>
2023-11-13 15:42:36 +0800

    fix: 新增识别古币

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import base64
 import openai
 import os
 from dotenv import load_dotenv
-import requests
 
 load_dotenv()
 
@@ -28,7 +27,8 @@ def run_by_video(video_path):
 def run_by_img(img_path):
     with open(img_path, "rb") as f:
         base64Frames = [base64.b64encode(f.read()).decode("utf-8")]
-    prompt = "这是一张关于12星座的描述图片，理解图片内容，并且生成一个简洁的短视频脚本，必须是中文，这对很重要"
+    prompt = "你是一名专业的古币鉴赏师，你有权利查看用户上传的古币照片，然后根据照片情况给出成色评分，你可以根据古币的边缘磨损、文字磨损、古币锈迹、沙眼气泡包浆等等多个维度，给出这枚古币的成色评分，满分是10分。请直接给出分值不要其他任何信息，分值表达形式如8.5分，9.0分。"
+    # prompt = "这是一张关于12星座的描述图片，理解图片内容，并且生成一个简洁的短视频脚本，必须是中文，这对很重要"
     return run(base64Frames, prompt, 1)
 
 
@@ -58,5 +58,5 @@ def run(base64Frames, prompt, steps=10):
 
 
 if __name__ == "__main__":
+    run_by_img("data/gb3.jpg")
     # run_by_video("data/cat.mp4")
-    run_by_img("data/2.png")
